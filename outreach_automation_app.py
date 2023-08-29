f9609ba09d6230e94956e53dcb4230881bba4094
KK <liqin.he.ken@gmail.com>
2023-08-29 00:22:41 -0400

    updated the step to choose whether include greet message

--- outreach_automation_app.py
+++ outreach_automation_app.py
@@ -10,14 +10,14 @@ def remove_special_characters_and_numbers(input_str):
     clean_str = re.sub(r'[^a-zA-Z\s]', '', input_str)
     return clean_str
 
-def create_outreach_record_df(input_data):
+def create_outreach_record_df(input_data, include_greet_meesage=False, greet_message=""):
     """
     This function takes in a string of outreach data and returns a DataFrame with the parsed data.
     """
 
     # Initialize an empty list to store the names
-    names_list = []
-    url_list = []
+    names_list, url_list = [],[]
+
     # Split the input string by lines
     for line in input_data.split("\n"):
         # Split each line by the '|' character
@@ -35,40 +35,51 @@ def create_outreach_record_df(input_data):
     outreach_record_df = pd.DataFrame({"Name":names_list, "Who?":by_who, "LinkedIn Profile":url_list})
 
     # Add a column with the greet message
-    outreach_record_df['greet_message'] = outreach_record_df['Name'].apply(lambda x: greet_message.replace('[firstname]', x.split(' ')[0]) if type(x) == str else np.nan)
+    if include_greet_meesage:
+        outreach_record_df['greet_message'] = outreach_record_df['Name'].apply(lambda x: greet_message.replace('[firstname]', x.split(' ')[0]) if type(x) == str else np.nan)
 
     return outreach_record_df
 
+greet_message = ""
+by_who = ""
 
 ### Streamlit App ###
 
 # Create a title and text
 st.title("Automate Your Linkedin Outreach")
-st.text("Will write instruction here")
+st.text("Need to download the One Tap extension first")
 
 
 st.markdown("## Step 1: Create a greet message")
 
 # Create a selectbox with a label and options
-selected_option = st.selectbox(
-    'You are?',
-    ('Others', 'Ken')
-)
+
 
 # Options for the user selected_option
-if selected_option == "Ken":
-    by_who = "Ken"
-    greet_message = "Hi, [firstname]! I am Ken, the co-founder at Esger, an NYC startup. We just raised $250k+ and support from top institutions like Cornell, Microsoft, and ERA. As we are using AI to tackle the heart of sustainability issues, the supply chain, I love to connect with experts in the field (like you!)."
-elif selected_option == "Others":
-    by_who = st.text_input("Whats your name:")
-    greet_message = st.text_area("(optional) Whats your greet message: Need to contain [firstname]")
+include_greet_message = st.checkbox("Include a greet message?")
 
-# check if greet_message is empty
-if greet_message == "":
-    greet_message = f"Hi, [firstname]! I am {by_who}, the co-founder at Esger, an NYC startup. We just raised $250k+ and support from top institutions like Cornell, Microsoft, and ERA. As we are using AI to tackle the heart of sustainability issues, the supply chain, I love to connect with experts in the field (like you!)."
-    st.write("Greet message (default):", greet_message)
-else :
-    st.write("The greet message is:", greet_message)
+
+if include_greet_message:
+    user_input = st.text_input("")
+
+    if user_input == "esger":
+        selected_option = st.selectbox('You are?', ('Others', 'Ken'))
+
+        if selected_option == "Ken":
+            by_who = "Ken"
+            greet_message = "Hi, [firstname]! I am Ken, the co-founder at Esger, an NYC startup. We just raised $250k+ and support from top institutions like Cornell, Microsoft, and ERA. As we are using AI to tackle the heart of sustainability issues, the supply chain, I love to connect with experts in the field (like you!)."
+        elif selected_option == "Others":
+            by_who = st.text_input("Whats your name:")
+            greet_message = st.text_area("(optional) Whats your greet message: Need to contain [firstname]")
+
+        # check if greet_message is empty
+        if greet_message == "":
+            greet_message = f"Hi, [firstname]! I am {by_who}, the co-founder at Esger, an NYC startup. We just raised $250k+ and support from top institutions like Cornell, Microsoft, and ERA. As we are using AI to tackle the heart of sustainability issues, the supply chain, I love to connect with experts in the field (like you!)."
+            st.write("Greet message (default):", greet_message)
+        else :
+            st.write("The greet message is:", greet_message)
+else:
+    by_who = st.text_input("You are?")
 
 
 st.write("")
@@ -77,7 +88,7 @@ st.markdown("## Step 2: Paste the URL and Export the CSV file")
 user_input = st.text_area("Exported URL from One Tap:")
 
 if st.button('Process the outreach data'):
-    outreach_record_df = create_outreach_record_df(user_input)
+    outreach_record_df = create_outreach_record_df(user_input, include_greet_message, greet_message)
     # Display the DataFrame
     st.write('Dataframe:', outreach_record_df)
 
